f2f00333aa9fdc0bebaa3eeb1f08c908e95285b9
Rajni Singh <rajnikantsingh.external@gmail.com>
2021-01-30 00:10:52 +1300

    added code to use each to loop

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -3,15 +3,22 @@
 describe("My First Test", () => {
 	it("Does not do much", () => {
 		expect(true).to.equal(true);
-    });
-    it("This will check the substring", () => {
-        expect('Rajni').to.contain('Raj');
-    });
-    it("Open a Test App", () => {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        cy.get(".search-keyword").type("ca");
-        cy.wait(2000);
-        cy.get(".products").find(".product").should("have.length", 4);
-        cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
-    })
+	});
+	it("This will check the substring", () => {
+		expect("Rajni").to.contain("Raj");
+	});
+	it("Open a Test App", () => {
+		cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+		cy.get(".search-keyword").type("ca");
+		cy.wait(2000);
+		cy.get(".products").find(".product").should("have.length", 4);
+		cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+		cy.get(".products")
+			.find(".product")
+			.each((element) => {
+				if (element.find(".product-name").text().includes("Cauliflower")) {
+					element.find("button").click();
+				}
+			});
+	});
 });
