fac9f7f03661b843b180f6a299947bbb23f7c9e8
Rajni Singh <rajnikantsingh.external@gmail.com>
2021-02-01 12:27:45 +1300

    added checkbox for multiple

--- cypress/integration/examples/AutomationPractise-1st Test.js
+++ cypress/integration/examples/AutomationPractise-1st Test.js
@@ -7,5 +7,8 @@ describe("Doing AUtomation Practice", () => {
 			.should("be.checked")
 			.and("have.value", "option1");
 		cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
+		cy.get('input[type="checkbox"]')
+			.check(["option2", "option1"])
+			.should("be.checked");
 	});
 });
