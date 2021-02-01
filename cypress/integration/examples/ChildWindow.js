700fd0ced655f92c34161c22fbc332aab533a312
Rajni Singh <rajnikantsingh.external@gmail.com>
2021-02-01 23:31:18 +1300

    added code to deal with ieFrames

--- cypress/integration/examples/ChildWindow.js
+++ cypress/integration/examples/ChildWindow.js
@@ -1,10 +1,20 @@
 /// <reference types="Cypress"/>
+/// <reference types="cypress - iframe"/>
+import "cypress-iframe";
 describe("Child Value", () => {
 	it("Get Attribute Value", () => {
 		cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-        cy.get("#opentab").then((e1) => {
-            const url = e1.prop("href");
-            cy.visit(url); // this url should be from same domain
-        });
-    });
+		cy.get("#opentab").then((e1) => {
+			const url = e1.prop("href");
+			cy.visit(url); // this url should be from same domain
+		});
+	});
+	it("handle child frame", () => {
+		// npm i -D cypress-iframe
+		cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+		cy.frameLoaded("#courses-iframe");
+		cy.iframe().find("a[href*='mentorship']").eq(0).click();
+		cy.iframe().find("h1[class*='pricing-title']").should("have.length", 2);
+	});
 });
